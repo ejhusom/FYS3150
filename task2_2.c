42a9d915727cf8bfb3714d4b088a13dc90f5c203
ejhusom <ejlgkvam@online.no>
2018-08-23 11:37:55 +0200

    write file

--- task2_2.c
+++ task2_2.c
@@ -1,1 +1,26 @@
+//  Created by Erik Johannes Husom on 26.06.2018.
+//  Copyright © 2018 Erik Johannes Husom. All rights reserved.
+
 #include <iostream>
+using namespace std;
+
+float sum_up(N) {
+    double sum;
+    sum = 0;
+    for (int i=1; i < N; i++){
+        double n;
+        n = i;
+        sum += 1/n;
+    }
+    return sum;
+}
+
+int main ()
+{
+    int N;
+    cout << "Insert N value";
+    cin >> N;
+    result = sum_up(N)
+    cout << result
+    return 0;
+}
