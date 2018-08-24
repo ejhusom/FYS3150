cd3d3ab97bf5e6555c04c3d4035d0a5f764be77a
ejhusom <ejlgkvam@online.no>
2018-08-24 10:25:53 +0200

    created project1

---
+++ fys3150_project1/main.cpp
@@ -0,0 +1,18 @@
+//==============================================================================
+// Title:           FYS3150 Project 1
+// Author:          Erik Johannes B. L. G. Husom
+// Date:            2018-08-24
+// Version:         0.1
+// Description:     One-dimensional Poisson-equation:
+//                  -u''(x) = f(x)
+//                  x = [0, 1]
+//                  u(0) = u(1) = 0
+//==============================================================================
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
