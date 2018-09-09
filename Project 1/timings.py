6af149f0abfb203f141a1c4939dc9a414cde2649
ejhusom <ejlgkvam@online.no>
2018-09-09 17:15:08 +0200

    small format changes

--- Project 1/timings.py
+++ Project 1/timings.py
@@ -20,13 +20,13 @@ data = np.loadtxt('timings.dat')
 outfile = open("benchmark.md","w")
 outfile.write("""# Benchmark timings of project 1
 
-Processor: Intel® Core™ M-5Y71 CPU @ 1.20GHz × 4
-Memory: 8 GB DDR3L SDRAM @ 1600 MHz - PC3L-12800
+- Processor: Intel® Core™ M-5Y71 CPU @ 1.20GHz × 4
+- Memory: 8 GB DDR3L SDRAM @ 1600 MHz - PC3L-12800
 
 |n    |General algorithm (s) |Specialized algorithm (s)|LU decomposition (s)|
 |-----|----------------------|-------------------------|----------------|\n""")
 for i in range(6):
-    outfile.write("|$10^{:d}$ ".format(i+1))
+    outfile.write("|10^{:d} ".format(i+1))
     outfile.write("| {:.7f} ".format(np.average(data[int(10*i):int(10*(i+1))])))
     outfile.write("| {:.7f} ".format(np.average(data[int(60+10*i):int(60+10*(i+1))])))
     if i<3:
