f9d8d6ddb48cb5f08374481b349eada72ea90b1d
ejhusom <ejlgkvam@online.no>
2018-09-09 21:42:50 +0200

    fine-tuning the plots

--- Project 1/dataplot.py
+++ Project 1/dataplot.py
@@ -13,8 +13,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 # SET PLOT STYLE
 plt.style.use('ggplot')
+plt.rcParams.update({'font.size': 8})
+w = 5; h = 2.5
 #==============================================================================
 # PLOT GENERAL ALGORITHM
+plt.figure(figsize=(w,h))
 data = np.loadtxt('genalgo1000.dat')
 x, u, v, eps = data[:,0], data[:,1], data[:,2], data[:,3]
 plt.plot(x,u,label='analytical')
@@ -25,9 +28,11 @@ for i in range(3):
 plt.xlabel('$x$')
 plt.ylabel('$u$')
 plt.legend()
-plt.show()
+plt.tight_layout()
+plt.savefig("/home/ejhusom/MEGAsync/FYS3150/project1/project1_report/genalgo.png",dpi=300)
 #==============================================================================
 # PLOT SPECIALIZED ALGORITHM
+plt.figure(figsize=(w,h))
 data = np.loadtxt('specalgo1000.dat')
 x, u, v, eps = data[:,0], data[:,1], data[:,2], data[:,3]
 plt.plot(x,u,label='analytical')
@@ -38,9 +43,20 @@ for i in range(6):
 plt.xlabel('$x$')
 plt.ylabel('$u$')
 plt.legend()
-plt.show()
+plt.tight_layout()
+plt.savefig("/home/ejhusom/MEGAsync/FYS3150/project1/project1_report/specalgo.png",dpi=300)
 #==============================================================================
-# PLOT LU ALGORITHM
-# data = np.loadtxt('lud.dat')
-# lux, luf = data[:,0], data[:,1]
-# plt.plot(lux,luf,label='LU-decomposition')
+# PLOT LU DECOMPOSITION
+plt.figure(figsize=(w,h))
+data = np.loadtxt('specalgo1000.dat') # for comparison
+x, u, v, eps = data[:,0], data[:,1], data[:,2], data[:,3]
+plt.plot(x,v,label='spec. alg. n=1000')
+for i in range(3):
+    data = np.loadtxt('lud{:d}.dat'.format(int(10**(i+1))))
+    x, v = data[:,0], data[:,1]
+    plt.plot(x,v,'--',label='n={:d}'.format(int(10**(i+1))))
+plt.xlabel('$x$')
+plt.ylabel('$u$')
+plt.legend()
+plt.tight_layout()
+plt.savefig("/home/ejhusom/MEGAsync/FYS3150/project1/project1_report/ludecomp.png",dpi=300)
