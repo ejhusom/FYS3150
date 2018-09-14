9bf8aea920c3f2f1ccb3be2173f54bc3ca43dcbd
ejhusom <ejlgkvam@online.no>
2018-09-14 09:33:46 +0200

    added high resolution timing

--- Project 2/project2.cpp
+++ Project 2/project2.cpp
@@ -13,9 +13,10 @@ USAGE:
 #include <fstream>
 #include <cmath>
 #include <iomanip>
-#include "time.h"
+#include <chrono>
 #include <armadillo>
 using namespace std;
+using namespace std::chrono;
 using namespace arma;
 
 void jacobi_method(int n);
@@ -65,11 +66,18 @@ void jacobi_method(int n){
   double max_iterations = double(n)*double(n)*double(n);
   int iterations = 0;
   double max_ = maxoffdiag(A, &k, &l, n);
+
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
   while (max_ > tolerance && iterations < max_iterations){
     max_ = maxoffdiag(A, &k, &l, n);
     rotate(A, k, l, n);
     iterations++;
   }
+  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+  duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+  cout << "Time used: " << time_span.count() << " seconds." << endl;
+
+  cout << "Number of iterations:" << iterations << endl;
 
   cout << "=========" << endl;
   cout << "Calculated eigenvalues:" << endl;
