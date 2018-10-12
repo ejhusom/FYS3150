1fcd7e800b1ada6214ee8fb2aaa0e05ca886d4dc
ejhusom <ejlgkvam@online.no>
2018-10-12 15:04:49 +0200

    started to make master integrator class

---
+++ project3/include/Integrator2.h
@@ -0,0 +1,27 @@
+// #ifndef INTEGRATOR2_H
+// #define INTEGRATOR2_H
+// #include <iostream>
+// #include <fstream>
+// #include <iomanip>
+// #include <cmath>
+// #include <string>
+// #include <vector>
+// #include "AstronomicalObject.h"
+// using namespace std;
+//
+// class Integrator2{
+// public:
+//   // Properties
+//   int MeshPoints;
+//   double TimeFinal;
+//   vector<AstronomicalObject> AllObjects;
+//   int Method;
+//   // Initializers
+//   Integrator2();
+//   Integrator2(int n, double t_f, vector<AstronomicalObject> objects, int m);
+//   // Functions
+//   void solve();
+//
+// };
+//
+// #endif /* INTEGRATOR2_H */
