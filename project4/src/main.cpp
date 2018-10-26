9ea9bfaf88361b3c38fae89fb134a0edcc0ce795
ejhusom <ejlgkvam@online.no>
2018-10-26 11:04:37 +0200

    random index generator working

--- project4/src/main.cpp
+++ project4/src/main.cpp
@@ -18,7 +18,7 @@ using namespace std;
 int main(int argc, char *argv[]){
   srand (time(NULL));
   // double randomint = (((double) rand() / (RAND_MAX)) > 0.5) ? 1: -1;
-  int dim = 100;
+  int dim = 2;
   // create and fill lattice with all spins up, plus ghost vectors for PBC
   double **Lattice = new double*[dim+2];
   for (int i = 0; i < dim+2; i++){
@@ -37,10 +37,13 @@ int main(int argc, char *argv[]){
     Lattice[0][i] = Lattice[dim][i];
   }
 
-
   double E = 0; double dE = 0;
   for (int i = 1; i < dim+1; i++) {
     for (int j = 1; j < dim+1; j++) {
+      // Find random indeces
+      x = 1 + (rand() % (int)(dim));
+      y = 1 + (rand() % (int)(dim));
+
       E += Lattice[i][j]*Lattice[i][j+1];
       E += Lattice[i][j]*Lattice[i+1][j];
     }
