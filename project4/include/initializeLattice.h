fc49ecc83396c9a50ea4197c0e801d903ae86a30
ejhusom <ejlgkvam@online.no>
2018-11-01 11:53:20 +0100

    added pointerlattice

--- project4/include/initializeLattice.h
+++ project4/include/initializeLattice.h
@@ -5,6 +5,6 @@
 #include <iomanip>
 using namespace std;
 
-void initializeLattice(double **Lattice, int dim, int state, double *E, double *M, std::mt19937_64 &gen, std::uniform_real_distribution<double> &distribution);
+void initializeLattice(double **Lattice, double ***PointerLattice, int dim, int state, double *E, double *M, std::mt19937_64 &gen, std::uniform_real_distribution<double> &distribution);
 
 #endif /* INITIALIZELATTICE_H */
--- project4/src/initializeLattice.cpp
+++ project4/src/initializeLattice.cpp
@@ -1,6 +1,6 @@
 #include "initializeLattice.h"
 
-void initializeLattice(double **Lattice, int dim, int state, double *E, double *M, std::mt19937_64 &gen, std::uniform_real_distribution<double> &distribution){
+void initializeLattice(double **Lattice, double ***PointerLattice, int dim, int state, double *E, double *M, std::mt19937_64 &gen, std::uniform_real_distribution<double> &distribution){
   // create and fill lattice with all spins up, plus ghost vectors for PBC
   if (state==1) {
     for(int i=1 ; i < dim+1 ; i++) {
@@ -17,17 +17,23 @@ void initializeLattice(double **Lattice, int dim, int state, double *E, double *
       }
     }
   }
-  // filling four "ghost vectors"
-  for (int i = 0; i < dim+1; i++) {
-    Lattice[i][dim+1] = Lattice[i][1];
-    Lattice[i][0] = Lattice[i][dim];
-    Lattice[dim+1][i] = Lattice[1][i];
-    Lattice[0][i] = Lattice[dim][i];
+  // Set elements of pointer lattice
+  for(int i=0 ; i < dim+2 ; i++) {
+    for(int j=0 ; j < dim+2 ; j++) {
+      PointerLattice[i][j] = &Lattice[i][j];
+    }
+  }
+  // filling four mirror elements
+  for (int i = 1; i < dim+1; i++) {
+    PointerLattice[i][dim+1] = &Lattice[i][1];
+    PointerLattice[i][0] = &Lattice[i][dim];
+    PointerLattice[dim+1][i] = &Lattice[1][i];
+    PointerLattice[0][i] = &Lattice[dim][i];
   }
   // finding initial energy
   for(int i=1 ; i < dim+1 ; i++) {
     for(int j=1 ; j < dim+1 ; j++) {
-      *E -= Lattice[i][j]*(Lattice[i+1][j]+Lattice[i][j+1]);
+      *E -= *PointerLattice[i][j]*(*PointerLattice[i+1][j]+*PointerLattice[i][j+1]);
     }
   }
 } // end of function initializeLattice
