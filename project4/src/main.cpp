cb4d5c7f753068d55bf5e0880c3a2cef112e606e
ejhusom <ejlgkvam@online.no>
2018-11-01 10:48:01 +0100

    succeded in creating pointer matrix

--- project4/src/main.cpp
+++ project4/src/main.cpp
@@ -24,9 +24,78 @@ int main(int argc, char *argv[]){
     case 2: dim = atoi(argv[1]);
   }
 
-  metropolis(dim, state, nCycles, T);
-  analyticalEnergy(T);
-  analyticalSpecificHeat(T);
+  // metropolis(dim, state, nCycles, T);
+  // analyticalEnergy(T);
+  // analyticalSpecificHeat(T);
+
+  int a = 4;
+  int n = 4;
+  double array[n] = {1,2,3,4};
+  // double *pointerarray = new double[n];
+  double *pointerarray[n];
+  for (int i = 0; i < n; i++) {
+    pointerarray[i] = &array[i];
+  }
+  pointerarray[0] = &array[2];
+  cout << "Array / Pointerarray\n";
+  for (int i=0; i<n; i++){
+    cout << setw(12) << setprecision(6) << array[i];
+    cout << setw(12) << setprecision(6) << *pointerarray[i] << endl;
+  }
+  array[2] = 100;
+  cout << "=============\n";
+  for (int i=0; i<n; i++){
+    cout << setw(12) << setprecision(6) << array[i];
+    cout << setw(12) << setprecision(6) << *pointerarray[i] << endl;
+  }
+  cout << "=====================================\n";
+  double **Lattice = new double*[dim+2];
+  for (int i = 0; i < dim+2; i++){
+    Lattice[i] = new double[dim+2];
+  }
+
+
+  for(int i=1 ; i < dim+1 ; i++) {
+    for(int j=1 ; j < dim+1 ; j++) {
+      Lattice[i][j] = 1;
+    }
+  }
+  for (int i=0; i<dim+2; i++){
+      for (int j=0; j<dim+2; j++){
+        cout << setw(8) << setprecision(4) << Lattice[i][j];
+      }
+    cout << endl;
+  }
+  cout << "============================\n";
+
+
+  double*** PointerLattice = new double**[dim+2];
+  for (int i = 0; i < dim+2; i++){
+    PointerLattice[i] = new double*[dim+2];
+  }
+
+  for(int i=0 ; i < dim+2 ; i++) {
+    for(int j=0 ; j < dim+2 ; j++) {
+      PointerLattice[i][j] = &Lattice[i][j];
+    }
+  }
+
+  for (int i=0; i<dim+2; i++){
+      for (int j=0; j<dim+2; j++){
+        // PointerLattice[i][j] = &Lattice[i][j];
+        cout << setw(8) << setprecision(4) << *PointerLattice[i][j];
+      }
+    cout << endl;
+  }
+  cout << "============================\n";
+  Lattice[0][0] = 1000;
+  for (int i=0; i<dim+2; i++){
+    for (int j=0; j<dim+2; j++){
+      // PointerLattice[i][j] = &Lattice[i][j];
+      cout << setw(8) << setprecision(4) << *PointerLattice[i][j];
+    }
+    cout << endl;
+  }
 
   return 0;
 } // end of main function
