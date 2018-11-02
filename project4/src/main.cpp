b15d2b838c40eed673277cd3ff0736ccfdf6b573
ejhusom <ejlgkvam@online.no>
2018-11-02 09:40:05 +0100

    parallelization on Monte Carlo cycles working

--- project4/src/main.cpp
+++ project4/src/main.cpp
@@ -5,6 +5,7 @@ Description:
 - Boltzmann constant = 1 -> temperature has dimension energy
 - Coupling constant J = 1.
 USAGE:
+- Run executable with mpirun -n 10 ./runproject4.x, where n is the number of processes
 =============================================================================*/
 #include "metropolis.h"
 #include "analytical.h"
@@ -12,6 +13,7 @@ using namespace std;
 ofstream outfile;
 
 void output(int dim, double temperature, double *ExpecVal, int nCycles, double timing){
+  for (int i = 0; i < 5; i++) ExpecVal[i] /= nCycles;
   double E_variance = (ExpecVal[1] - ExpecVal[0]*ExpecVal[0])/dim/dim;
   double M_variance = (ExpecVal[3] - ExpecVal[2]*ExpecVal[2])/dim/dim;
   outfile << setw(15) << setprecision(8) << temperature;
@@ -43,12 +45,14 @@ int main(int argc, char *argv[]){
   double temp_step = 0.1;
 
   double *ExpecVal = new double[5];
+  double *TotalExpecVal = new double[5];
+  for (int i = 0; i < 5; i++) TotalExpecVal[i] = 0;
   double timing;
 
-  int numprocs, my_rank;
+  int nProcs, my_rank;
 
   MPI_Init (&argc, &argv);
-  MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_size (MPI_COMM_WORLD, &nProcs);
   MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
 
   MPI_Bcast (&dim, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -56,24 +60,35 @@ int main(int argc, char *argv[]){
   MPI_Bcast (&temp_final, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
   MPI_Bcast (&temp_step, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-  outfile.open("Lattices" + to_string(dim) + ".dat");
-  outfile << setw(15) << setprecision(8) << "T";
-  outfile << setw(15) << setprecision(8) << "E";
-  outfile << setw(15) << setprecision(8) << "E2";
-  outfile << setw(15) << setprecision(8) << "M";
-  outfile << setw(15) << setprecision(8) << "M2";
-  outfile << setw(15) << setprecision(8) << "M abs";
-  outfile << setw(15) << setprecision(8) << "M variance";
-  outfile << setw(15) << setprecision(8) << "C_V";
-  outfile << setw(15) << setprecision(8) << "Run time";
-  outfile << setw(15) << setprecision(8) << "No. of cycles" << endl;
+  int cycleInterval = nCycles/nProcs;
+  int loopStart = my_rank*cycleInterval;
+  int loopStop = (my_rank+1)*cycleInterval;
+
+  if (my_rank == 0){
+    outfile.open("Lattice" + to_string(dim) + "Cycles" + to_string(nCycles) + ".dat");
+    outfile << setw(15) << setprecision(8) << "T";
+    outfile << setw(15) << setprecision(8) << "E";
+    outfile << setw(15) << setprecision(8) << "E2";
+    outfile << setw(15) << setprecision(8) << "M";
+    outfile << setw(15) << setprecision(8) << "M2";
+    outfile << setw(15) << setprecision(8) << "M abs";
+    outfile << setw(15) << setprecision(8) << "M variance";
+    outfile << setw(15) << setprecision(8) << "C_V";
+    outfile << setw(15) << setprecision(8) << "Run time";
+    outfile << setw(15) << setprecision(8) << "No. of cycles" << endl;
+  }
+  double TimeStart, TimeEnd, TotalTime;
+  TimeStart = MPI_Wtime();
   for (double temperature = temp_init; temperature <= temp_final; temperature+=temp_step) {
-    metropolis(dim, state, nCycles, temperature, ExpecVal, &timing);
-    output(dim, temperature, ExpecVal, nCycles, timing);
-    cout << "T=" << temperature << " done...\n";
+    metropolis(dim, state, loopStart, loopStop, temperature, ExpecVal, &timing);
+    for (int i=0; i<5; i++) MPI_Reduce(&ExpecVal[i], &TotalExpecVal[i], 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (my_rank==0) output(dim, temperature, TotalExpecVal, nCycles, timing);
+    if (my_rank==0) cout << "T=" << temperature << " done...\n";
   }
   outfile.close();
-
+  TimeEnd = MPI_Wtime();
+  TotalTime = TimeEnd-TimeStart;
+  if (my_rank==0) cout << "Time used: " << TotalTime << endl << "No. of processes: " << nProcs << endl;
 
   MPI_Finalize ();
 
