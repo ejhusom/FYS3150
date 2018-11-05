5e2a8beac81ae79bb38def9d405398af8cea1ac3
ejhusom <ejlgkvam@online.no>
2018-11-05 12:41:53 +0100

    added acceptations counter

--- project4/include/metropolis.h
+++ project4/include/metropolis.h
@@ -8,6 +8,6 @@
 #include "initializeLattice.h"
 using namespace std;
 
-void metropolis(int dim, int state, int loopStart, int loopStop, double T, double *E, double *M, double *ExpecVal, double **Lattice, double ***PointerLattice, std::mt19937_64 &gen, std::uniform_real_distribution<double> &distribution);
+void metropolis(int dim, int state, int loopStart, int loopStop, double T, double *E, double *M, double *ExpecVal, double **Lattice, double ***PointerLattice, std::mt19937_64 &gen, std::uniform_real_distribution<double> &distribution, int *counter);
 
 #endif /* METROPOLIS_H */
--- project4/src/metropolis.cpp
+++ project4/src/metropolis.cpp
@@ -1,6 +1,6 @@
 #include "metropolis.h"
 
-void metropolis(int dim, int state, int loopStart, int loopStop, double T, double *E, double *M, double *ExpecVal, double **Lattice, double ***PointerLattice, std::mt19937_64 &gen, std::uniform_real_distribution<double> &distribution){
+void metropolis(int dim, int state, int loopStart, int loopStop, double T, double *E, double *M, double *ExpecVal, double **Lattice, double ***PointerLattice, std::mt19937_64 &gen, std::uniform_real_distribution<double> &distribution, int *counter){
   double *w = new double[17];
   for (int i = 0; i < 17; i+=4) w[i] = exp(-(i-8)/T);
 
@@ -12,6 +12,7 @@ void metropolis(int dim, int state, int loopStart, int loopStop, double T, doubl
       y = 1 + (int) (distribution(gen)*(double)dim);
       dE = 2**PointerLattice[x][y]*(*PointerLattice[x-1][y]+*PointerLattice[x+1][y]+*PointerLattice[x][y-1]+*PointerLattice[x][y+1]);
       if (distribution(gen) <= w[dE+8]) {
+        *counter += 1;
         Lattice[x][y] *= -1;  // flip the spin
         *M += 2**PointerLattice[x][y]; // update magnetization
         *E += (double) dE;     // update energy
